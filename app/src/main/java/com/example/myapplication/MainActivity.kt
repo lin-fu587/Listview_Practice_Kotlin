ee76cbd1ceda38437c33bc33bc56d7b7e62ce48c
lin-fu587 <jim80129@gmail.com>
2020-11-23 22:42:56 +0800

    first

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         var name: String? = null
     }
 
-    inner class MyAdapter(val data: Array<Data?>, val view: Int) : BaseAdapter() {
+    inner class MyAdapter(private val data: Array<Data?>, private val view: Int) : BaseAdapter() {
         override fun getCount(): Int {
             return data.size
         }
@@ -25,13 +26,13 @@ class MainActivity : AppCompatActivity() {
             return 0
         }
 
-        override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             var convertView = convertView
-            convertView = layoutInflater.inflate(view, parent, false)
+            convertView = LayoutInflater.from(parent!!.context).inflate(view, parent, false)
             val name = convertView.findViewById<TextView>(R.id.name)
-            name.text = data[position]?.name
+            name.text = data[position]!!.name
             val imageView = convertView.findViewById<ImageView>(R.id.imageView)
-            imageView.setImageResource(data[position]?.photo!!)
+            imageView.setImageResource(data[position]!!.photo)
             return convertView
         }
     }
@@ -42,8 +43,7 @@ class MainActivity : AppCompatActivity() {
         val transNameArray = arrayOf("腳踏車", "機車", "汽車", "巴士")
         val transPhotoIdArray = intArrayOf(R.drawable.trans1, R.drawable.trans2, R.drawable.trans3, R.drawable.trans4)
         val transData = arrayOfNulls<Data>(transNameArray.size)
-        for (i in 0 until transData.size)
-        {
+        for (i in 0 until transData.size) {
             transData[i] = Data()
             transData[i]!!.name = transNameArray[i]
             transData[i]!!.photo = transPhotoIdArray[i]
@@ -51,17 +51,14 @@ class MainActivity : AppCompatActivity() {
         val transAdapter: MyAdapter = MyAdapter(transData, R.layout.trans_list)
         val spinner = findViewById<Spinner>(R.id.spinner)
         spinner.adapter = transAdapter
-
         val messageArray = arrayOf("訊息1", "訊息2", "訊息3", "訊息4", "訊息5", "訊息6")
         val messageAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, messageArray)
         val listView = findViewById<ListView>(R.id.listView)
         listView.adapter = messageAdapter
-
         val cubeeNameArray = arrayOf("哭哭", "發抖", "再見", "生氣", "昏倒", "竊笑", "很棒", "你好", "驚嚇", "大笑")
         val cubeePhotoIdArray = intArrayOf(R.drawable.cubee1, R.drawable.cubee2, R.drawable.cubee3, R.drawable.cubee4, R.drawable.cubee5, R.drawable.cubee6, R.drawable.cubee7, R.drawable.cubee8, R.drawable.cubee9, R.drawable.cubee10)
         val cubeeData = arrayOfNulls<Data>(cubeeNameArray.size)
-        for (i in 0 until cubeeData.size)
-        {
+        for (i in 0 until cubeeData.size) {
             cubeeData[i] = Data()
             cubeeData[i]!!.name = cubeeNameArray[i]
             cubeeData[i]!!.photo = cubeePhotoIdArray[i]
